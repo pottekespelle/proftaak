b3d1515640efd571661e488b11eb32ba7598f04d
[Niels] <[niellogics@gmail.com]>
2017-05-10 11:10:15 +0200

    begin styling

--- index.php
+++ index.php
@@ -2,8 +2,31 @@
 <html>
 <head>
 	<title>Proftaak 2k17</title>
+	<link rel="stylesheet" type="text/css" href="css/headerStyle.css">
 </head>
 <body>
+
+	<?php 
+		include "inc/header.php";
+	 ?>
+
+
+	<div class="header_Container">
+		<!-- links logo -> Home -> zoeken -> inlog knop -->
+	</div>
+
+	<div id="container">
+
+		<div id="search-container">
+			<!-- search for steam ID's -->
+			<div id="search-Input-Container">
+				<input type="text" name="" class="Search-input">
+				<button class="Search-input" id="search-Button">Search</button>
+			</div>
+			
+		</div>
+
+	</div>
 
 </body>
 </html>
