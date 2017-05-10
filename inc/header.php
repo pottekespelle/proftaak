4f3ea2e7ecddde94ca50b288ab7542f395cc3356
[Niels] <[niellogics@gmail.com]>
2017-05-10 12:00:27 +0200

    dingen gedaan

--- inc/header.php
+++ inc/header.php
@@ -1,11 +1,7 @@
 <div id="top-nav" class="nav">
 
 		<div class="top">
-			<div class="wrapper">
-				<a class="Links" id="HomeImg" href="Index.php">Logo</a>
-				<a class="Links" id="Home" href="Index.php">Home</a>
-				<a class="Links" id="Contact" href="contact.php">Contact</a>
-				<a class="Links" id="Projecten" href="index.php">Projecten!</a>
-			</div>
+			<a id="logoLAMG" href="Index.php"><img src="img/logo.png" id="logoImg"></a>
+				
 		</div>
 </div>
